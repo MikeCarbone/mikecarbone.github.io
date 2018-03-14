cf60f9a46a692df558a16353aec64bc4c49425bf
mike-carbone <mfcbone@gmail.com>
2018-03-14 18:23:10 -0400

    triggering animation in skills section on scroll

--- static/js/fancy.js
+++ static/js/fancy.js
@@ -57,3 +57,36 @@ function hideProject(){
 	shade.classList.add('makeLight');
 	setTimeout(function(){ shade.style.display = 'none'; }, 500);
 }
+
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.skillFill .level');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
+
+// Capture scroll events
+$(window).scroll(function(){
+    checkAnimation();
+});
