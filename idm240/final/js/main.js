5c00691fbb6de032a844865ef0e768d496a29fd5
mike-carbone <mfcbone@gmail.com>
2018-06-13 21:23:50 -0400

    removed tilting effect

--- idm240/final/js/main.js
+++ idm240/final/js/main.js
@@ -21,24 +21,6 @@ function volumeButtonControls(sound){
     return isMuted;
 }
 
-window.addEventListener("mousemove",function(e) {
-
-    if (window.innerWidth <= "500px"){
-      var width = window.innerWidth;
-      var height = window.innerHeight;
-      var clientHeight = document.body.clientHeight;
-      var skew = {}
-      skew.y = (1.5 * ((e.x / width) - 0.5)) 
-      skew.x = -(1.5 * ((e.y / height) - 0.5));
-
-      document.body.style.webkitTransform = "scale(1.01) perspective("+clientHeight+"px) rotateX("+skew.x+"deg) rotateY("+skew.y+"deg)";
-      document.getElementById('posty-js').style.webkitTransform = "translateY(75px) translateX(25px) translateZ(70px) scale(1.7) perspective("+clientHeight+"px) rotateX("+(-skew.x*15)+"deg) rotateY("+(-skew.y*15) +"deg)";
-    }
-  
-})
-
-
-
 //Fades in all elements on the page after they are loaded
 window.onload = fadeIn();
 function fadeIn(){
