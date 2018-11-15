f7dbab9fe65e4c12d99191f52491d7f75715bea9
mike-carbone <mfcbone2@gmail.com>
2018-11-15 01:17:32 -0500

    add socializr

---
+++ socializr/build/precache-manifest.26df1143ce58dbc3788da132add88ac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a960c5d6a3248a2c46b9",
+    "url": "/static/js/main.a960c5d6.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "a960c5d6a3248a2c46b9",
+    "url": "/static/css/main.88e83585.chunk.css"
+  },
+  {
+    "revision": "45c65bb0ce944a344bf5fb9787decd60",
+    "url": "/index.html"
+  }
+];
